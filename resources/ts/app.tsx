e228cd98020d3d416030b45968e6ba214d8440d2
aokisinn <aokisinn1826@gmail.com>
2021-02-12 23:02:56 +0900

    Initial commit

---
+++ resources/ts/app.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App: React.FC = () => {
+    return <div>Hello World</div>;
+};
+
+if (document.getElementById("app")) {
+    ReactDOM.render(<App />, document.getElementById("app"));
+}
