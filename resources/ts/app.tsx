1784f1f21b75fb6739f52ee67ab792bde14f06e4
aokisinn <aokisinn1826@gmail.com>
2021-02-12 23:47:27 +0900

    react route　追加

--- resources/ts/app.tsx
+++ resources/ts/app.tsx
@@ -1,12 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Button from "react-bootstrap/Button";
+// import Button from "react-bootstrap/Button";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 const App: React.FC = () => {
     return (
-        <div>
-            Hello World<Button variant="primary">Primary</Button>
-        </div>
+        <Router>
+            <nav>
+                <ul>
+                    <Link to="/">
+                        <li>Top</li>
+                    </Link>
+                    <Link to="/login">
+                        <li>login</li>
+                    </Link>
+                    <Link to="/signup">
+                        <li>signup</li>
+                    </Link>
+                </ul>
+            </nav>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/signup" component={Signup} />
+            </Switch>
+        </Router>
     );
 };
 
---
+++ resources/ts/pages/Signup.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Signup = (props: any) => {
+    return (
+        <div>
+            <h1>signup</h1>
+        </div>
+    );
+};
+
+export default Signup;
