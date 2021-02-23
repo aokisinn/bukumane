03e11ea9a434ab984299c3d2d7576ba7539af9e1
aokisinn <aokisinn1826@gmail.com>
2021-02-23 11:21:53 +0900

    apiClient設定追加

--- resources/ts/utils/apiClient.tsx
+++ resources/ts/utils/apiClient.tsx
@@ -2,7 +2,11 @@ import axios from "axios";
 
 const apiClient = axios.create({
     baseURL: process.env.MIX_API_BASE_URL,
-    withCredentials: true
+    withCredentials: true,
+    headers: {
+        "Content-Type": "application/json",
+        "X-Requested-With": "XMLHttpRequest"
+    }
 });
 
 export default apiClient;
